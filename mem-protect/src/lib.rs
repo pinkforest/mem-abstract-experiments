d1440469320316e1db86dbb314c2698ac5354a43
pinkforest <36498018+pinkforest@users.noreply.github.com>
2023-09-23 15:30:25 +1000

    Add basic implementation

--- mem-protect/src/lib.rs
+++ mem-protect/src/lib.rs
@@ -1,14 +1,71 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(docsrs, feature(doc_auto_cfg, doc_cfg, doc_cfg_hide))]
+#![cfg_attr(docsrs, doc(cfg_hide(docsrs)))]
+//------------------------------------------------------------------------
+// Documentation:
+//------------------------------------------------------------------------
+#![doc(
+    html_logo_url = "https://cdn.jsdelivr.net/gh/pinkforest/mem-protect/docs/assets/mem-protect-logo-clear.png"
+)]
+#![doc = include_str!("../README.md")]
+//------------------------------------------------------------------------
+// Linting:
+//------------------------------------------------------------------------
+#![cfg_attr(allow_unused_unsafe, allow(unused_unsafe))]
+#![deny(
+    clippy::undocumented_unsafe_blocks,
+    clippy::unwrap_used,
+    missing_docs,
+    rust_2018_idioms,
+    unused_lifetimes,
+    unused_qualifications
+)]
+
+// Slice can be lockable
+// Warning: Scratch .. sketching out
+// Should these be instead in some higher level crate ?
+// TODO: Do these traits make any sense ? :D
+// TODO: Can these fail ? And why
+/// TODO: DOC
+pub trait MemLockable {
+    /// TODO: DOC
+    fn lock<T: Sized>(_t: &T);
+    /// TODO: DOC
+    fn unlock<T: Sized>(_t: &T);
+}
+/// TODO: DOC
+pub trait MemLockableSlice {
+    /// TODO: DOCK
+    fn lock_slice<T: Sized>(_t: &[T]);
+    /// TODO: DOCK
+    fn unlock_slice<T: Sized>(_t: &[T]);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// TODO: What we should do between raw/low level vs higher level abstractions ?
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//------------------------------------------------------------------------
+// Select implementation to which to abstract and expose over
+//------------------------------------------------------------------------
+#[cfg(mem_protect_methods = "libc_mlock")]
+mod libc_mlock;
+#[cfg(mem_protect_methods = "libc_mlock")]
+pub use libc_mlock::*;
+
+#[cfg(mem_protect_methods = "winapi_mlock")]
+mod winapi_mlock;
+#[cfg(mem_protect_methods = "winapi_mlock")]
+pub use winapi_mlock::*;
+
+// TODO: check if mem_protect_build_effects = "strict" and compile_error in case this is hit
+/*
+#[cfg(not(all(
+    mem_protect_methods = "libc_mlock",
+    mem_protect_methods = "winapi_mlock"
+)))]
+mod dummy_mlock;
+#[cfg(not(all(
+    mem_protect_methods = "libc_mlock",
+    mem_protect_methods = "winapi_mlock"
+)))]
+pub use dummy_mlock::*;
+*/
---
+++ mem-protect/src/libc_mlock.rs
@@ -0,0 +1,101 @@
+//! libc mlock
+
+use super::*;
+use core::mem;
+
+// Dummy types.
+struct MemLocking;
+
+impl MemLockable for MemLocking {
+    fn lock<T: Sized>(m: &T) {
+        let ptr: *const T = m;
+        let size = mem::size_of::<T>();
+
+        // SAFETY: TODO
+        unsafe {
+            libc::mlock(ptr as *mut libc::c_void, size);
+            #[cfg(any(target_os = "freebsd", target_os = "dragonfly"))]
+            #[no_coverage]
+            libc::madvise(ptr, size, libc::MADV_NOCORE);
+            #[cfg(target_os = "linux")]
+            libc::madvise(ptr as *mut libc::c_void, size, libc::MADV_DONTDUMP);
+        }
+    }
+    fn unlock<T: Sized>(m: &T) {
+        let ptr: *const T = m;
+        let size = mem::size_of::<T>();
+
+        // SAFETY: TODO
+        unsafe {
+            libc::munlock(ptr as *mut libc::c_void, size);
+            #[cfg(any(target_os = "freebsd", target_os = "dragonfly"))]
+            #[no_coverage]
+            libc::madvise(ptr, size, libc::MADV_CORE);
+            #[cfg(target_os = "linux")]
+            libc::madvise(ptr as *mut libc::c_void, size, libc::MADV_DODUMP);
+        }
+    }
+}
+
+impl MemLockableSlice for MemLocking {
+    fn lock_slice<T: Sized>(m: &[T]) {
+        let size = mem::size_of_val(m);
+        let ptr = m.as_ptr() as *mut libc::c_void;
+
+        // SAFETY: TODO
+        unsafe {
+            libc::mlock(ptr, size);
+
+            #[cfg(any(target_os = "freebsd", target_os = "dragonfly"))]
+            #[no_coverage]
+            libc::madvise(ptr, size, libc::MADV_NOCORE);
+
+            #[cfg(target_os = "linux")]
+            libc::madvise(ptr, size, libc::MADV_DONTDUMP);
+        }
+    }
+    fn unlock_slice<T: Sized>(m: &[T]) {
+        let size = mem::size_of_val(m);
+        let ptr = m.as_ptr() as *mut libc::c_void;
+        
+        // SAFETY: TODO
+        unsafe {
+            libc::munlock(ptr, size);
+
+            #[cfg(any(target_os = "freebsd", target_os = "dragonfly"))]
+            #[no_coverage]
+            libc::madvise(ptr, size, libc::MADV_CORE);
+
+            #[cfg(target_os = "linux")]
+            libc::madvise(ptr, size, libc::MADV_DODUMP);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_lock_u8() {
+        let a: u8 = 0;
+        MemLocking::lock(&a);
+    }
+
+    #[test]
+    fn test_unlock_u8() {
+        let a: u8 = 0;
+        MemLocking::unlock(&a);
+    }
+
+    #[test]
+    fn test_lock_slice() {
+        let a: [u8; 2] = [0, 0];
+        MemLocking::lock_slice(&a);
+    }
+    #[test]
+    fn test_unlock_slice() {
+        let a: [u8; 2] = [0, 0];
+        MemLocking::unlock_slice(&a);
+    }
+}
